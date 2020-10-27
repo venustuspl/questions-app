1509fce135524d3b7900ab0e536c137d39823bb4
venustuspl <venustus.pl@gmail.com>
2020-10-27 21:58:47 +0100

    QuestionDraft v1

--- 100ideas/src/main/java/pl/stormit/ideas/questions/domain/Question.java
+++ 100ideas/src/main/java/pl/stormit/ideas/questions/domain/Question.java
@@ -1,4 +1,79 @@
 package pl.stormit.ideas.questions.domain;
 
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
 public class Question {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private String body;
+    private Integer categoryId;
+    private LocalDateTime creationDate;
+
+    public Question() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Question question = (Question) o;
+        return Objects.equals(id, question.id) &&
+                Objects.equals(body, question.body) &&
+                Objects.equals(categoryId, question.categoryId) &&
+                Objects.equals(creationDate, question.creationDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, body, categoryId, creationDate);
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "id=" + id +
+                ", body='" + body + '\'' +
+                ", categoryId=" + categoryId +
+                ", creationDate=" + creationDate +
+                '}';
+    }
 }
