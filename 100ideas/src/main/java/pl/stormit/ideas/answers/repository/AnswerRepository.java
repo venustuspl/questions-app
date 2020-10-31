540f956d2ad37a732c6212500d32c6614ef98dc5
Bartłomiej Kiliszek <bartekkiliszek@gmail.com>
2020-10-31 17:55:39 +0100

    feature: Add AnswerRepository interface

---
+++ 100ideas/src/main/java/pl/stormit/ideas/answers/repository/AnswerRepository.java
@@ -0,0 +1,11 @@
+package pl.stormit.ideas.answers.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import pl.stormit.ideas.answers.domain.Answer;
+
+import java.util.UUID;
+
+@Repository
+public interface AnswerRepository extends CrudRepository<Answer, UUID> {
+}
---
+++ 100ideas/src/main/java/pl/stormit/ideas/answers/service/AnswerService.java
@@ -0,0 +1,2 @@
+package pl.stormit.ideas.answers.service;public class AnswerService {
+}
