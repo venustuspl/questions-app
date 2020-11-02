d61de21a3f3cf2e42176675e8704c9ecb536b0d5
Bartłomiej Kiliszek <bartekkiliszek@gmail.com>
2020-11-02 07:21:10 +0100

    test: Add AnswerService tests files

---
+++ 100ideas/src/test/java/pl/stormit/ideas/answers/service/AnswerServiceIntegrationTests.java
@@ -0,0 +1,13 @@
+package pl.stormit.ideas.answers.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnswerServiceIntegrationTests {
+
+    @Autowired
+    private AnswerService answerService;
+
+}
