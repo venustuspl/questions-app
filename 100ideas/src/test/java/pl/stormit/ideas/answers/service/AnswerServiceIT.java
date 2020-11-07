81bf9db4c6f158345fe9fb89ff2b5be50ddc0eb5
Bartłomiej Kiliszek <bartekkiliszek@gmail.com>
2020-11-07 23:13:49 +0100

    test: Add example test

--- 100ideas/src/test/java/pl/stormit/ideas/answers/service/AnswerServiceIT.java
+++ 100ideas/src/test/java/pl/stormit/ideas/answers/service/AnswerServiceIT.java
@@ -1,7 +1,13 @@
 package pl.stormit.ideas.answers.service;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import pl.stormit.ideas.answers.domain.Answer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 
 @SpringBootTest
 class AnswerServiceIT {
@@ -9,4 +15,17 @@ class AnswerServiceIT {
     @Autowired
     private AnswerService answerService;
 
+    @Test
+    void shouldSaveAnswerInDB() {
+        //given
+        Answer answerToSave = new Answer();
+        answerToSave.setBody("Test body");
+
+        //when
+        Answer savedAnswer = answerService.addAnswer(answerToSave);
+
+        //then
+        assertEquals("Test body", savedAnswer.getBody());
+        assertNotNull(savedAnswer.getId());
+    }
 }
