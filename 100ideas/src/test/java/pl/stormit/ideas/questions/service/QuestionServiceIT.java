32c9d0e9535ccbbffa8c488cd81f23129601a8e9
venustuspl <venustus.pl@gmail.com>
2020-11-13 23:24:19 +0100

    added test shouldUpdateQuestionInDB

--- 100ideas/src/test/java/pl/stormit/ideas/questions/service/QuestionServiceIT.java
+++ 100ideas/src/test/java/pl/stormit/ideas/questions/service/QuestionServiceIT.java
@@ -1,10 +1,16 @@
 package pl.stormit.ideas.questions.service;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import pl.stormit.ideas.questions.domain.Question;
+import pl.stormit.ideas.questions.repository.QuestionRepository;
 
+import java.time.OffsetDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -14,6 +20,9 @@ class QuestionServiceIT {
 
     @Autowired
     private QuestionService questionService;
+
+    @Autowired
+    private QuestionRepository questionRepository;
 
     @Test
     void shouldSaveQuestionInDB() {
@@ -25,7 +34,34 @@ class QuestionServiceIT {
         Question savedQuestion = questionService.addQuestion(questionToSave);
 
         //then
-        assertEquals("Test body", savedQuestion.getName());
+        assertEquals("Test name", savedQuestion.getName());
         assertNotNull(savedQuestion.getId());
+    }
+
+    @Test
+    void shouldUpdateQuestionInDB() {
+        //given
+        Question savedQuestion = questionRepository.save(getQuestionToSave());
+        savedQuestion.setName("Updated name");
+
+        //when
+        questionService.updateQuestion(savedQuestion);
+
+        //then
+        Question updatedQuestion = questionRepository.findById(savedQuestion.getId()).orElseThrow();
+        assertThat(updatedQuestion.getName()).isEqualTo("Updated name");
+        assertThat(updatedQuestion.getId()).isEqualTo(savedQuestion.getId());
+    }
+
+    @BeforeEach
+    @AfterEach
+    void clear() {
+        questionRepository.deleteAll();
+    }
+
+    private Question getQuestionToSave() {
+        Question question = questionRepository.save(new Question());
+        question.setCreationDate(OffsetDateTime.now());
+        return question;
     }
 }
