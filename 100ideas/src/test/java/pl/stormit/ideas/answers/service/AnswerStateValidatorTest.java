0682b06c33637bfc423cdec52c650d1057c87871
Bartłomiej Kiliszek <bartekkiliszek@gmail.com>
2020-11-15 17:29:48 +0100

    test: Add unit tests for AnswerStateValidator

---
+++ 100ideas/src/test/java/pl/stormit/ideas/answers/service/AnswerStateValidatorTest.java
@@ -0,0 +1,144 @@
+package pl.stormit.ideas.answers.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pl.stormit.ideas.answers.domain.Answer;
+import pl.stormit.ideas.questions.domain.Question;
+import pl.stormit.ideas.questions.repository.QuestionRepository;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class AnswerStateValidatorTest {
+    private AnswerStateValidator answerStateValidator;
+    private final QuestionRepository questionRepository = mock(QuestionRepository.class);
+
+    @BeforeEach
+    void setUp() {
+        answerStateValidator = new AnswerStateValidator(questionRepository);
+    }
+
+    @Test
+    void shouldThrowExceptionDuringAddingWhenAnswerHasId() {
+        //given
+        Answer answer = getAnswerWithId();
+
+        //when
+        //then
+        assertThatIllegalStateException()
+                .isThrownBy(() -> answerStateValidator.validateForAdding(answer));
+    }
+
+    @Test
+    void shouldThrowExceptionDuringAddingWhenAnswerHasQuestionWithoutId() {
+        //given
+        Answer answer = new Answer();
+        answer.setQuestion(new Question());
+
+        //when
+        //then
+        assertThatIllegalStateException()
+                .isThrownBy(() -> answerStateValidator.validateForAdding(answer));
+    }
+
+    @Test
+    void shouldThrowExceptionDuringAddingWhenAnswerHasQuestionThatIsNotInDB() {
+        //given
+        Answer answer = getAnswerWithQuestion();
+        when(questionRepository.findById(any(UUID.class))).thenReturn(Optional.empty());
+
+        //when
+        //then
+        assertThatExceptionOfType(NoSuchElementException.class)
+                .isThrownBy(() -> answerStateValidator.validateForAdding(answer));
+    }
+
+    @Test
+    void shouldNotThrowExceptionDuringAddingWhenAnswerHasNoIdAndHasQuestionThatIsInDB() {
+        //given
+        Answer answer = getAnswerWithQuestion();
+        when(questionRepository.findById(any(UUID.class))).thenReturn(Optional.of(answer.getQuestion()));
+
+        //when
+        //then
+        assertThatCode(() -> answerStateValidator.validateForAdding(answer))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    void shouldThrowExceptionDuringAddingWhenAnswerHasNoQuestion() {
+        //given
+        Answer answer = new Answer();
+
+        //when
+        //then
+        assertThatIllegalStateException()
+                .isThrownBy(() -> answerStateValidator.validateForAdding(answer));
+    }
+
+    @Test
+    void shouldNotThrowExceptionDuringUpdatingWhenAnswerHasId() {
+        //given
+        Answer answer = getAnswerWithId();
+
+        //when
+        //then
+        assertThatCode(() -> answerStateValidator.validateForUpdating(answer))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    void shouldThrowExceptionDuringUpdatingWhenAnswerHasNoId() {
+        //given
+        Answer answer = new Answer();
+
+        //when
+        //then
+        assertThatIllegalStateException()
+                .isThrownBy(() -> answerStateValidator.validateForUpdating(answer));
+    }
+
+    @Test
+    void shouldNotThrowExceptionDuringDeletingWhenAnswerHasId() {
+        //given
+        Answer answer = getAnswerWithId();
+
+        //when
+        //then
+        assertThatCode(() -> answerStateValidator.validateForDeleting(answer))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    void shouldThrowExceptionDuringDeletingWhenAnswerHasNoId() {
+        //given
+        Answer answer = new Answer();
+
+        //when
+        //then
+        assertThatIllegalStateException()
+                .isThrownBy(() -> answerStateValidator.validateForDeleting(answer));
+    }
+
+    private Answer getAnswerWithId() {
+        Answer answer = new Answer();
+        answer.setId(UUID.randomUUID());
+        return answer;
+    }
+
+    private Answer getAnswerWithQuestion() {
+        Answer answer = new Answer();
+        Question question = new Question();
+        question.setId(UUID.randomUUID());
+        answer.setQuestion(question);
+        return answer;
+    }
+}
