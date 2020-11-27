097f99deb199e495430e4e6f7b93396f7c662203
Bartłomiej Kiliszek <bartekkiliszek@gmail.com>
2020-11-27 22:48:00 +0100

    feat: Add getAnswer method to Answer Controller

--- 100ideas/src/main/java/pl/stormit/ideas/answers/controller/AnswerController.java
+++ 100ideas/src/main/java/pl/stormit/ideas/answers/controller/AnswerController.java
@@ -1,8 +1,13 @@
 package pl.stormit.ideas.answers.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.stormit.ideas.answers.service.AnswerService;
+
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/answers")
@@ -11,5 +16,12 @@ public class AnswerController {
 
     public AnswerController(AnswerService answerService) {
         this.answerService = answerService;
+    }
+
+
+    @GetMapping("/{id}")
+    public String getAnswer(Model model, @PathVariable UUID id) {
+        model.addAttribute("answer", answerService.getAnswerById(id));
+        return "answer/answer";
     }
 }
