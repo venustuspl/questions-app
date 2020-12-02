9e28b15a323cdc80d4a1e3521f9d5d553be18aac
Bartłomiej Kiliszek <bartekkiliszek@gmail.com>
2020-12-02 13:05:03 +0100

    feat: Added Answer Mapper
    Mapper maps Answer to DTOs and vice versa

---
+++ 100ideas/src/main/java/pl/stormit/ideas/answers/utils/AnswerMapper.java
@@ -0,0 +1,51 @@
+package pl.stormit.ideas.answers.utils;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+import pl.stormit.ideas.answers.domain.Answer;
+import pl.stormit.ideas.answers.domain.dto.AnswerRequest;
+import pl.stormit.ideas.answers.domain.dto.AnswerResponse;
+import pl.stormit.ideas.answers.domain.dto.AnswerUpdatedRequest;
+import pl.stormit.ideas.answers.service.AnswerService;
+import pl.stormit.ideas.questions.domain.Question;
+import pl.stormit.ideas.questions.service.QuestionService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class AnswerMapper {
+    private final AnswerService answerService;
+    private final QuestionService questionService;
+
+    public AnswerMapper(AnswerService answerService, QuestionService questionService) {
+        this.answerService = answerService;
+        this.questionService = questionService;
+    }
+
+    public  AnswerResponse mapToAnswerResponse(Answer answer) {
+        AnswerResponse answerResponse = new AnswerResponse();
+        BeanUtils.copyProperties(answer, answerResponse);
+        return answerResponse;
+    }
+
+    public List<AnswerResponse> mapToAnswerResponseList(List<Answer> answers) {
+        return answers.stream()
+                .map(this::mapToAnswerResponse)
+                .collect(Collectors.toList());
+    }
+
+    public Answer mapToAnswer(AnswerRequest answerRequest) {
+        Question question = questionService.getQuestionById(answerRequest.getQuestionId());
+        Answer answer = new Answer();
+        answer.setQuestion(question);
+        answer.setBody(answerRequest.getBody());
+        return answer;
+    }
+
+    public Answer mapToAnswer(AnswerUpdatedRequest answerUpdatedRequest) {
+        Answer answer = answerService.getAnswerById(answerUpdatedRequest.getId());
+        answer.setBody(answerUpdatedRequest.getBody());
+        return answer;
+    }
+}
