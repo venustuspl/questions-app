01ed2c803af352edf008d506c7fc491de9be2aef
Bartłomiej Kiliszek <bartekkiliszek@gmail.com>
2020-12-06 13:30:01 +0100

    feat: Changed CrudRepository to JpaRepository
    Jpa will help with paging

--- 100ideas/src/main/java/pl/stormit/ideas/answers/repository/AnswerRepository.java
+++ 100ideas/src/main/java/pl/stormit/ideas/answers/repository/AnswerRepository.java
@@ -1,6 +1,6 @@
 package pl.stormit.ideas.answers.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.stormit.ideas.answers.domain.Answer;
 
@@ -8,6 +8,6 @@ import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface AnswerRepository extends CrudRepository<Answer, UUID> {
+public interface AnswerRepository extends JpaRepository<Answer, UUID> {
     List<Answer> findAllByQuestionIdOrderByCreationDateDesc(UUID questionId);
 }
