2f72ba10d60f2d4e1b59aecd020a47056ff13d6c
Bartłomiej Kiliszek <bartekkiliszek@gmail.com>
2020-12-15 16:23:46 +0100

    test: Added missing tests for AnswerService

--- 100ideas/src/test/java/pl/stormit/ideas/answers/service/AnswerServiceTest.java
+++ 100ideas/src/test/java/pl/stormit/ideas/answers/service/AnswerServiceTest.java
@@ -1,15 +1,20 @@
 package pl.stormit.ideas.answers.service;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.AdditionalAnswers;
 import pl.stormit.ideas.answers.domain.Answer;
 import pl.stormit.ideas.answers.repository.AnswerRepository;
 import pl.stormit.ideas.answers.service.validator.AnswerValidator;
+import pl.stormit.ideas.questions.domain.Question;
 import pl.stormit.ideas.questions.repository.QuestionRepository;
 
 import java.time.OffsetDateTime;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -131,6 +136,63 @@ class AnswerServiceTest {
         //then
         assertThatCode(() -> answerService.deleteAnswer(answer))
                 .doesNotThrowAnyException();
+    }
+
+    @Test
+    void shouldReturnAnswerWhenAnswerIdIsPassed() {
+        //given
+        when(answerRepository.findById(any(UUID.class))).thenReturn(Optional.of(new Answer()));
+
+        //when
+        Answer answer = answerService.getAnswerById(UUID.randomUUID());
+
+        //then
+        assertThat(answer).isNotNull();
+    }
+
+    @Test
+    void shouldReturnExceptionWhenPassedAnswerIdIsNotPresentInDB() {
+        //given
+        UUID answerId = UUID.randomUUID();
+        when(answerRepository.findById(any(UUID.class))).thenReturn(Optional.empty());
+
+        //when
+        Throwable throwable = Assertions.catchThrowable(() -> answerService.getAnswerById(answerId));
+
+        //then
+        assertThat(throwable)
+                .hasMessage("The Answer object with id " + answerId.toString() + " does not exist in DB");
+    }
+
+    @Test
+    void shouldReturnAnswerListWhenQuestionIdIsPassed() {
+        //given
+        Question question = new Question();
+        question.setId(UUID.randomUUID());
+        when(questionRepository.findById(any(UUID.class))).thenReturn(Optional.of(question));
+        when(questionRepository.findAllByCategoryIdOrderByCreationDateDesc(any(UUID.class)))
+                .thenReturn(Collections.emptyList());
+        //when
+        List<Answer> answers = answerService.getAllAnswersByQuestionId(UUID.randomUUID());
+
+        //then
+        assertThat(answers).isNotNull();
+    }
+
+    @Test
+    void shouldReturnExceptionWhenPassedQuestionIdIsNotPresentInDB() {
+        //given
+        Question question = new Question();
+        UUID questionId = UUID.randomUUID();
+        question.setId(questionId);
+        when(questionRepository.findById(any(UUID.class))).thenReturn(Optional.empty());
+
+        //when
+        Throwable throwable = Assertions.catchThrowable(() ->  answerService.getAllAnswersByQuestionId(questionId));
+
+        //then
+        assertThat(throwable)
+                .hasMessage("The Question object with id " + questionId + " does not exist in DB");
     }
 
     private Answer getAnswerWithSecondsSinceCreation(long seconds) {
