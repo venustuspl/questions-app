a39d2d527c01439863797b5414c432b3a70df9e1
venustuspl <venustus.pl@gmail.com>
2020-12-21 22:34:27 +0100

    Added test shouldReturnSingleQuestionView and shouldReturnQuestionAddView for QuestionController

--- 100ideas/src/it/java/pl/stormit/ideas/questions/controller/QuestionControllerTest.java
+++ 100ideas/src/it/java/pl/stormit/ideas/questions/controller/QuestionControllerTest.java
@@ -9,16 +9,17 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import pl.stormit.ideas.answers.domain.dto.AnswerResponse;
 import pl.stormit.ideas.categories.domain.Category;
 import pl.stormit.ideas.categories.service.CategoryService;
 import pl.stormit.ideas.questions.domain.QuestionResponse;
+import pl.stormit.ideas.questions.domain.QuestionUpdatedRequest;
 import pl.stormit.ideas.questions.mapper.QuestionMapper;
 import pl.stormit.ideas.questions.service.QuestionService;
 
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import static org.hamcrest.Matchers.emptyCollectionOf;
 import static org.mockito.Mockito.when;
@@ -37,6 +38,9 @@ class QuestionControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    public static final UUID ID = UUID.randomUUID();
+    public static final String ID_IN_STRING = ID.toString();
 
     public static List<QuestionResponse> getQuestionResponseList() {
         return new ArrayList<>();
@@ -63,8 +67,48 @@ class QuestionControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("question/questions"))
                 .andExpect(model().attribute("questions", emptyCollectionOf(QuestionResponse.class)))
-                .andExpect(model().attribute("categories", emptyCollectionOf(AnswerResponse.class)))
+                .andExpect(model().attribute("categories", emptyCollectionOf(Category.class)))
                 .andExpect(model().attribute("offsetDateTime", Matchers.any(OffsetDateTime.class)));
+    }
 
+    @Test
+    void shouldReturnQuestionAddView() throws Exception {
+        //given
+        when(categoryService.getAllCategories()).thenReturn(getCategories());
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .get("/questions/add");
+
+        //when
+        ResultActions result = mockMvc.perform(request);
+
+        //then
+        result
+                .andExpect(status().isOk())
+                .andExpect(view().name("question/questionadd"))
+                .andExpect(model().attribute("categories", emptyCollectionOf(Category.class)));
+    }
+
+    @Test
+    void shouldReturnSingleQuestionView() throws Exception {
+        //given
+        when(questionMapper.mapQuestionToQuestionResponse(questionService.getQuestionById(ID))).thenReturn(new QuestionResponse());
+        when(categoryService.getAllCategories()).thenReturn(getCategories());
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .get("/questions/" + ID_IN_STRING);
+
+        //when
+        ResultActions result = mockMvc.perform(request);
+
+        //then
+        result
+                .andExpect(status().isOk())
+                .andExpect(view().name("question/questionupdate"))
+                .andExpect(model().attribute("question", Matchers.any(QuestionResponse.class)))
+                .andExpect(model().attribute("questionToUpdate", Matchers.any(QuestionUpdatedRequest.class)))
+                .andExpect(model().attribute("categories", emptyCollectionOf(Category.class)))
+                .andExpect(model().attribute("exception", false))
+                .andExpect(model().attribute("exceptionEdit", false));
     }
 }
